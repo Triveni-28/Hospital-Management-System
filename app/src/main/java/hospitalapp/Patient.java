a5025e5dd69c48ca99cac08dccdf591e073bef70
Triveni G <tg1686222@gmail.com>
2025-07-20 11:44:34 +0530

    Update Patient.java

--- app/src/main/java/hospitalapp/Patient.java
+++ app/src/main/java/hospitalapp/Patient.java
@@ -1,10 +1,14 @@
-public class Patient {
+import java.io.Serializable;
+
+public class Patient implements Serializable {
     public String name;
     public int age;
     public int emergencyLevel;
     public long timestamp;
 
-    public Patient() {} // Firebase requires empty constructor
+    public Patient() {
+        // Default constructor (optional, useful for Firebase or deserialization)
+    }
 
     public Patient(String name, int age, int level, long ts) {
         this.name = name;
